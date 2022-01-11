4d077975b642a784a72335e188be752c8010838f
s.miroshnikov <97443660+s-miroshnikov@users.noreply.github.com>
2022-01-11 17:09:18 +0300

    slices

--- 01.go
+++ 01.go
@@ -3,18 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]string
-	a[0] = "Hello"
-	a[1] = "World"
-	fmt.Println(a[0], a[1])
-	fmt.Println(a)
-
 	primes := [6]int{2, 3, 5, 7, 11, 13}
-	fmt.Println(primes)
 
-	fmt.Println(len(primes))
-	primes = [6]int{}
+	var s []int = primes[1:4]
+	fmt.Println(s)
+
+	fmt.Println(len(s), cap(s))
+	s[0] = 8
+
 	fmt.Println(primes)
-	b := [...]bool{true, true, true}
-	fmt.Printf("%T", b)
 }
