a150aa8866ff95af5bb48fbeb5a51f00955435c3
s.miroshnikov <97443660+s-miroshnikov@users.noreply.github.com>
2022-01-12 15:59:36 +0300

    Update methods.go

--- methods/methods.go
+++ methods/methods.go
@@ -12,8 +12,17 @@ type Vertex struct {
 func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
-
+func (Vertex) Hi() {
+	fmt.Println("Hello!")
+}
+func (v *Vertex) AddX() {
+	v.X += 10
+}
 func main() {
 	v := Vertex{33, 25}
 	fmt.Println(v.Abs())
+	v.Hi()
+	x := v
+	x.AddX()
+	fmt.Println(x)
 }
