5275349a13cb4443272e9d62f68460696052ec94
s.miroshnikov <97443660+s-miroshnikov@users.noreply.github.com>
2022-02-09 23:58:39 +0300

    pgx conn

--- final-script/main.go
+++ final-script/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	
+	"database/sql"
+	
+	"log"
+	"github.com/jackc/pgx/v4"
+)
 
+func main() {
+	connectionString := "host=db user=worker1 password=xxxxx dbname=library sslmode=disable"
+	db, err := sql.Open( driverName: "pgx", connectionString)
+    if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
 }
