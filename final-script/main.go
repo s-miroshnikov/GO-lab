182f767df46650264f59c1b86ffb05efcca221f3
s.miroshnikov <97443660+s-miroshnikov@users.noreply.github.com>
2022-02-10 00:42:13 +0300

    add types

--- final-script/main.go
+++ final-script/main.go
@@ -5,7 +5,7 @@ import (
 	"database/sql"
 	
 	"log"
-	"github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 func main() {
@@ -18,4 +18,24 @@ func main() {
 	if err := db.Ping(); err != nil {
 		log.Fatal(err)
 	}
+	type Articles struct {
+		id        int64
+		magazines_id	int64
+		article_type_id	int64
+		author_id	int64
+	  }
+	  
+	  type magazines struct {
+		id   int64
+		name string
+	  }
+	  
+	  type article_types struct {
+		id      int64
+		"type"    string
+	  }
+	  type author struct {
+		id   int64
+		author string
+	  }
 }
